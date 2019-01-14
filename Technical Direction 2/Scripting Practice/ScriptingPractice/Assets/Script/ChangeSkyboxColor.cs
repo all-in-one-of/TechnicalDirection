b3e6c865368efeeceba55551ac10129d900d7e6f
Kendall <kw.silent.winds@gmail.com>
2019-01-14 15:38:00 -0700

    Skybox Scripting Work

---
+++ Technical Direction 2/Scripting Practice/ScriptingPractice/Assets/Script/ChangeSkyboxColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkyboxColor : MonoBehaviour
+{
+
+	public Color SkyboxColor;
+
+	public void ColorSkybox()
+	{
+	    //make a random, vector3 value so it can act as the color setting
+
+	    RenderSettings.skybox.SetColor("_Tint", SkyboxColor);
+	}
+}
---
+++ Technical Direction 2/Scripting Practice/ScriptingPractice/Assets/Script/RotateSkybox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour {
+
+public bool setRot = false;
+public float rotSpeed = 0.3f;
+
+
+    //If setRot is true, the Sky will spin.
+    public void Update()
+    {
+        if (setRot == true)
+            RenderSettings.skybox.SetFloat("_Rotation", rotSpeed*Time.time);
+    }
+
+    //If button is pushed, it changes the value of setRot
+    public void ToggleSkyRotation()
+    {
+        setRot = !setRot;
+    }
+}
