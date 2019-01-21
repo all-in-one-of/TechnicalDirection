54fc777abe5ae39fb930942bd6251002e8112b8d
KendallWeaver <flaze5@gmail.com>
2019-01-21 15:00:40 -0700

    Update Player.cs

--- Technical Direction 2/Scripting Practice/ScriptingPractice/Assets/Script/Player.cs
+++ Technical Direction 2/Scripting Practice/ScriptingPractice/Assets/Script/Player.cs
@@ -7,5 +7,31 @@ public class Player : MonoBehaviour{
 
   public int level = 3;
   public int health = 40;
+  
+  //These functions will be used for the button events. 
+  
+  //This will save the player's information
+  public void SavePlayer()
+  {
+    SaveSystem.SavePlayer(this);
+  }
+  
+  //This will load the data from the file to the player as a data type.  
+  //Then the rest of the variables will be applied.
+  public void LoadPlayer()
+  {
+    PlayerData data = SaveSystem.LoadPlayer();
+    
+    level = data.level;
+    health = data.health;
+    
+    Vector3 position;
+    position.x = data.position[0];
+    position.y = data.position[1];
+    position.z = data.position[2];
+    transform.position = position;
+    
+  }
+  
 
 }
