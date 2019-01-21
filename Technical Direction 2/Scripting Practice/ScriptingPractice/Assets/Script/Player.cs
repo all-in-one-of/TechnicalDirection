1632532f41e1a8b4277df041b71d9dee339548d5
KendallWeaver <flaze5@gmail.com>
2019-01-21 14:31:08 -0700

    Create Player.cs

---
+++ Technical Direction 2/Scripting Practice/ScriptingPractice/Assets/Script/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Colelctions.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+//This script was used for the PlayerData script, learning how to make a save and load script.
+
+  public int level = 3;
+  public int health = 40;
+
+}
